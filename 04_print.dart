d866d3e0f34d68892bb8fa4b51f8544cb5e452a2
Farooquekk <22sw040@students.muet.edu.pk>
2025-07-21 08:46:20 -0700

    basic print examples in Dart
    Introduces a new Dart file demonstrating basic print statements, including printing strings and variables.

---
+++ 04_print.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Basic print statement
+  print('Hello Dart');
+
+  //  Print Var
+  int age = 20;
+  String name = 'Farooque Sajjad';
+  print('My name is $name and I am $age years old.');
+
+  
+}
