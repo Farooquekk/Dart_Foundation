4074ae1d7835e37c14c9629630dc501a2d11c57d
Farooquekk <22sw040@students.muet.edu.pk>
2025-07-22 07:50:49 -0700

    Example of operators in Dart
    Introduces a new Dart file demonstrating arithmetic, comparison, and logical operators with sample print statements.

---
+++ 05_operators.dart
@@ -0,0 +1,23 @@
+void main() {
+  // Arithmetic operators --> +,-,*,/,%
+  int x = 3;
+  int y = 10;
+  print(x + y);
+  print(x - y);
+  print(x * y);
+  print(x / y);
+  print(x % y);
+
+  // Comparison Operators --> ==, !=, >, <, >=, <=
+  print(2 > 3);
+  print(2 >= 1); // follows or logic
+  print(x == y);
+  print('1' == 1);
+  print(y >= x);
+  print(y <= x);
+
+  // Logical Operators --> &&,||,!
+  
+
+  print('');
+}
