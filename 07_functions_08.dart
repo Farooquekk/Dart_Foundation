b6b0565cfbbb88302f77c442669d2103b51f4882
Farooquekk <22sw040@students.muet.edu.pk>
2025-07-31 09:30:45 -0700

    Function returning greeting message
    Introduces a getGreetings function that returns a personalized greeting. Demonstrates usage by printing the greeting for 'Farooque'.

---
+++ 07_functions_08.dart
@@ -0,0 +1,12 @@
+// function returning value
+
+void main() {
+  String getGreetings(String name) {
+    return ("Hello $name What's up!! ");
+  }
+
+  print(getGreetings("Farooque"));
+  var output = getGreetings("Farooque");
+  print(output);
+  
+}
