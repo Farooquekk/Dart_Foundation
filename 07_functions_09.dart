f237fa287bb0d86c14a0672fcd82fde886e789a6
Farooquekk <22sw040@students.muet.edu.pk>
2025-08-01 10:36:33 -0700

    Example of anonymous functions in Dart
    Introduces a Dart script demonstrating the use of anonymous functions (lambdas) as callbacks with the forEach method on a list.

---
+++ 07_functions_09.dart
@@ -0,0 +1,9 @@
+// Anonymous functions(Lambda) --> useful in callbacks
+
+void main() {
+  var list = [40, "Farooque Sajjad", "Muet", 3.97, true];
+
+  list.forEach((ele) {
+    print("** $ele **");
+  });
+}
